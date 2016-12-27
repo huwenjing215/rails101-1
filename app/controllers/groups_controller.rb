9f9fb99ece82cbbf45f0049b4d499ed5f9738a2e
huwenjing <huwenjing215@gmail.com>
2016-12-27 10:44:52 +0800

    implement group relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,51 +8,51 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-def show
+  def show
   @group = Group.find(params[:id])
-  @posts = @group.posts.recent
-end
+  @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
+  end
 
-def edit
-end
+  def edit
+  end
 
   def create
     @group = Group.new(group_params)
     @group.user = current_user
 
     if @group.save
-redirect_to groups_path
-else
+      redirect_to groups_path
+    else
   render :new
-end
-end
+  end
+  end
 
-def update
+  def update
 
-if @group.update(group_params)
-redirect_to groups_path,notice: "Update Success"
-else
+    if @group.update(group_params)
+      redirect_to groups_path,notice: "Update Success"
+    else
   render :edit
-end
-end
+  end
+  end
 
-def destroy
-  @group.destroy
-  redirect_to groups_path  alert:"Group deleted"
-end
+  def destroy
+    @group.destroy
+    redirect_to groups_path  alert:"Group deleted"
+  end
 
   private
 
-def find_group_and_check_permission
-  @group = Group.find(params[:id])
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
 
-  if current_user !=@group.user
+    if current_user !=@group.user
     redirect_to root_path,alert: "you have no permission"
   end
-end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
   end
 
-end
+  end
